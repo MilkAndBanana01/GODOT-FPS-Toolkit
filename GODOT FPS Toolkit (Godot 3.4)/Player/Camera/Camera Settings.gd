5c58c0606557db861254733ce3552891d876b749
MilkAndBanana01 <94128784+MilkAndBanana01@users.noreply.github.com>
2022-08-21 13:39:38 +0800

    Camera panning added

--- GODOT FPS Toolkit (Godot 3.4)/Player/Camera/Camera Settings.gd
+++ GODOT FPS Toolkit (Godot 3.4)/Player/Camera/Camera Settings.gd
@@ -3,6 +3,8 @@ extends Node
 export var enable := true
 export var mouse_sensitivity := 1.0
 
+onready var head = Ap.player.get_node("Head")
+
 func _ready() -> void:
 	if enable:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- GODOT FPS Toolkit (Godot 3.4)/Player/Camera/Panning/Panning.gd
+++ GODOT FPS Toolkit (Godot 3.4)/Player/Camera/Panning/Panning.gd
@@ -1,16 +1,19 @@
 extends "res://Player/Camera/Camera Settings.gd"
 
 export var smoothing_enabled := false
-export var smoothing_amount := 25.0
+export(float,0,10) var smoothing_amount := 5.0
 
 var mouse_movement : Vector2
-var rotation_velocity : Vector3
+var rotation_velocity : Vector2
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		mouse_movement = event.relative
 
 func _physics_process(delta: float) -> void:
-	rotation_velocity = rotation_velocity.linear_interpolate(Vector3(mouse_movement.x,0,mouse_movement.y) * mouse_sensitivity, delta)
+	rotation_velocity = rotation_velocity.linear_interpolate(Vector2(mouse_movement.x,mouse_movement.y) * mouse_sensitivity, 
+	delta * (10.1 - smoothing_amount) if smoothing_enabled else 1)
 	Ap.player.rotate_y(-deg2rad(rotation_velocity.x))
+	head.rotate_x(-deg2rad(rotation_velocity.y))
+	head.rotation.x = clamp(head.rotation.x,deg2rad(-90),deg2rad(90))
 	mouse_movement = Vector2.ZERO
